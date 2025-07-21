cade6db4dd435a3d980593dc0d8aaac37bebc839
Delon <nicolas123delon@gmail.com>
2025-07-21 14:37:36 +0300

    feat(Max): add -Infinity symbol

--- src/components/CustomTable.vue
+++ src/components/CustomTable.vue
@@ -9,13 +9,15 @@
       <thead>
         <tr>
           <th></th>
-          <th v-for="rowName in matrix.rows[0].rowName">{{ rowName }}</th>
+          <th v-for="row in matrix.rows">{{ row.name }}</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(row, index) in matrix.rows">
-          <th v-if="row.rowName">{{ row.rowName[index] }}</th>
-          <td v-for="val in row.data">{{ val != Infinity ? val : "+∞" }}</td>
+          <th v-if="row.name">{{ row.name }}</th>
+          <td v-for="val in row.data">
+            {{ val == Infinity ? "+∞" : val == -Infinity ? "-∞" : val }}
+          </td>
         </tr>
       </tbody>
     </table>
