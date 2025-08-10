cd51c8d1d6696ddb8c9aa28e8c02e8905cb94832
Delon <nicolas123delon@gmail.com>
2025-08-10 10:32:30 +0300

    feat(demoucron) : lenght of node

--- src/components/CustomNode.vue
+++ src/components/CustomNode.vue
@@ -10,13 +10,7 @@ defineProps<NodeProps>();
     <div class="label text-bold text-black">{{ data.label }}</div>
 
     <Handle type="target" :position="Position.Left" />
-    <Handle type="source" :position="Position.Left" />
-    <Handle type="target" :position="Position.Right" />
     <Handle type="source" :position="Position.Right" />
-    <Handle type="target" :position="Position.Top" />
-    <Handle type="source" :position="Position.Top" />
-    <Handle type="target" :position="Position.Bottom" />
-    <Handle type="source" :position="Position.Bottom" />
   </div>
 </template>
 
@@ -24,21 +18,19 @@ defineProps<NodeProps>();
 @import "@vue-flow/core/dist/style.css";
 
 .circle-node {
-  width: 64px;
-  height: 64px;
-  border: 4px solid black;
+  width: 52px;
+  height: 52px;
+  border: 4px solid grey;
   border-radius: 360px;
   background-color: #f9f5ff;
   display: flex;
   align-items: center;
   justify-content: center;
-  /* box-shadow: 2px 2px 16px rgba(0, 0, 0, 0.9); */
 }
 
 .label {
   text-align: center;
   text-transform: uppercase;
   font-size: x-large;
-  /* background-color: linear-gradient(to right, #074e53, #0752f3); */
 }
 </style>
